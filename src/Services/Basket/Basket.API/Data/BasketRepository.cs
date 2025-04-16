8f8b2b42978c34353875ae561dce346e87780ded
Camilo Cadavid - Ceiba Software <camilo.cadavid@ceiba.com.co>
2025-04-16 13:02:52 -0500

    agrego la data, excepciones y modelos y agrego configuraciones de Basket.API

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,33 @@
+﻿using Basket.API.Exceptions;
+using Basket.API.Models;
+using BuildingBlocks.Exceptions;
+using Marten;
+
+namespace Basket.API.Data
+{
+    public class BasketRepository(IDocumentSession sesion) : IBasketRepository
+    {
+
+
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            var basket = await sesion.LoadAsync<ShoppingCart>(userName, cancellationToken);
+
+            return basket is null ? throw new BasketNotFoundException(userName):basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            sesion.Store(basket);
+            await sesion.SaveChangesAsync();
+            return basket;
+        }
+
+        public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            sesion.Delete<ShoppingCart>(userName);
+            await sesion.SaveChangesAsync(cancellationToken);
+            return true;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,35 @@
+using Basket.API.Data;
+using Basket.API.Models;
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+using Carter;
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandler(options => { });
+
+app.MapCarter();
 
 app.Run();
